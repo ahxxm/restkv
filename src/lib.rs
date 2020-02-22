a6c3e4fdd0499dac0103eb79dde98aebc5f029bb
ahxxm <i@ahxxm.com>
2020-02-22 19:45:53 +0800

    init commit

---
+++ src/lib.rs
@@ -0,0 +1,147 @@
+extern crate pretty_env_logger;
+extern crate rand;
+
+use lazy_static;
+
+use std::iter;
+use std::sync::{Arc, RwLock};
+
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+use regex::Regex;
+
+use bytes::Bytes;
+use kv::{Config, Store};
+use log::{warn};
+
+// db and bucket names
+static TOKEN: &str = "token";
+static VALUES: &str = "values";
+lazy_static::lazy_static! {
+    static ref DB: Arc<RwLock<Store>> = {
+        let cfg = Config {
+            path: std::path::PathBuf::from("kv.db"),
+            read_only: false,
+            temporary: false,
+            use_compression: true,
+            flush_every_ms: Some(30000),
+        };
+        let store = Store::new(cfg).unwrap();
+        Arc::new(RwLock::new(store))
+    };
+}
+
+pub fn token_exist(t: &str) -> bool {
+    let token_key = format!("token-{}", t);
+    if let Ok(db) = DB.read() {
+        if let Ok(bucket) = db.bucket::<String, String>(Some(TOKEN)) {
+            if let Ok(b) = bucket.contains(&token_key) {
+                return b;
+            }
+        }
+    }
+    true
+}
+
+pub fn write_token(t: &str) -> bool {
+    let token_key = format!("token-{}", t);
+    if let Ok(db) = DB.write() {
+        if let Ok(bucket) = db.bucket::<String, String>(Some(TOKEN)) {
+            if let Ok(_) = bucket.set(token_key, "1") {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+pub fn get_value(t: String, k: String) -> String {
+    let null = "".to_string();
+    if !token_exist(&t) {
+        return null;
+    }
+
+    let db = match DB.read() {
+        Ok(d) => d,
+        Err(e) => {
+            warn!("failed get read lock {}", e);
+            return null;
+        }
+    };
+
+    let bucket = match db.bucket::<String, String>(Some(VALUES)) {
+        Ok(b) => b,
+        Err(e) => {
+            warn!("failed get values bucket {}", e);
+            return null;
+        }
+    };
+
+    let tk = format!("{}-{}", t, k);
+    if let Ok(Some(val)) = bucket.get(tk) {
+        val
+    } else {
+        null
+    }
+}
+
+fn validate_key(key: &str) -> bool {
+    lazy_static::lazy_static! {
+        static ref RE: Regex = Regex::new(r"[0-9a-zA-Z]{1,100}").unwrap();
+    }
+    RE.is_match(key)
+
+}
+
+pub fn post_value(token: String, key: String, value: Bytes) -> String {
+    let null = "".to_string();
+    if !token_exist(&token) || !validate_key(&key){
+        return null;
+    }
+
+    let body = match String::from_utf8(value.to_vec()) {
+        Ok(s) => s,
+        Err(e) => {
+            warn!("invalid utf8 string {}", e);
+            return null;
+        }
+    };
+    let db = match DB.write() {
+        Ok(d) => d,
+        Err(e) => {
+            warn!("failed get read lock {}", e);
+            return null;
+        }
+    };
+
+    let bucket = match db.bucket::<String, String>(Some(VALUES)) {
+        Ok(b) => b,
+        Err(e) => {
+            warn!("failed get values bucket {}", e);
+            return null;
+        }
+    };
+
+    let tk = format!("{}-{}", token, key);
+    if let Ok(_) = bucket.set(tk, body) {
+        return key;
+    }
+    null
+}
+
+pub fn random_token() -> String {
+    let len = 8;
+    let mut token: String;
+
+    loop {
+        let mut rng = thread_rng();
+        token = iter::repeat(())
+            .map(|()| rng.sample(Alphanumeric))
+            .take(len)
+            .collect();
+        if !token_exist(&token) {
+            break;
+        }
+    }
+    token
+}
---
+++ src/main.rs
@@ -0,0 +1,41 @@
+extern crate pretty_env_logger;
+
+mod lib;
+use lib::{get_value, post_value, random_token, write_token};
+
+use log::info;
+use warp::Filter;
+
+/*
+TODO: /stats
+*/
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+    // TODO: limit global rate?
+    let new_token = warp::path!("new").and(warp::post()).map(|| {
+        let token = random_token();
+        let written = write_token(&token);
+        if written {
+            token
+        } else {
+            "".to_string()
+        }
+    });
+
+    let get = warp::path!(String / String).and(warp::get()).map(get_value);
+
+    // TODO: a-zA-Z0-9 key
+    let post = warp::path!(String / String)
+        .and(warp::post())
+        .and(warp::body::content_length_limit(1024 * 4)) // limit to 4KB body
+        .and(warp::body::bytes())
+        .map(post_value);
+
+    // TODO: /stats
+    let routes = new_token.or(get).or(post);
+
+    info!("starting server at {}", 8080);
+    warp::serve(routes).run(([0, 0, 0, 0], 8080)).await;
+}
